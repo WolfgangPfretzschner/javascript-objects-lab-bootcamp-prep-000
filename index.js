a866da81c02199c65f228e98a40be5fa07d4fbe7
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-03-29 09:13:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,24 +10,11 @@ function updateObjectWithKeyAndValue(object, key, value){
       return object;
   }
  function deleteFromObjectByKey(object, key){
-   var newObj = object.assign({object},object);
-   
+   var newObj = object.assign({},object);
+   delete newObj[key]
+   return newObj
  }
 
-  function destructivelyDeleteFromObjectByKey(object, key)', function() {
-    it('returns object without the delete key/value pair', function() {
-      var obj = { prop: 1 }
-      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-      expect(newObj['prop']).toBe(undefined)
-    })
-
-    it('modifies the original object', function() {
-      var obj = { prop: 1 }
-      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-      expect(obj['prop']).toBe(undefined)
-    })
-  })
-
-})
+  function destructivelyDeleteFromObjectByKey(object, key){
+    
+  }
